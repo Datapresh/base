95fddbf1919f80500bf46c3d9df10bde1e42b45b
Datapresh <ace22140005@noun.edu.ng>
2022-06-18 12:18:36 +0200

    from my system

---
+++ index.js
@@ -0,0 +1,12 @@
+function Add(params) {
+    console.log(params.a + params.b)
+}
+
+function Minus(params) {
+    console.log(params.a - params.b)
+}
+
+
+function Div(params) {
+    console.log(params.a / params.b)
+}
